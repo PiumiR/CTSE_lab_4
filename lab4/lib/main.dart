7c8031359dae47fea7989d376e2f7cf3fbe15845
Piumi Ranawansha <piumiranawansha@gmail.com>
2022-03-11 06:36:35 +0530

    completed the CTSE lab sheet 4

---
+++ lab4/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lab4/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+
